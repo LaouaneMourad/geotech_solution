599446d2e9f220a4a73f97ea70030bf604fdb82b
mourad <laouanemourad@gmail.com>
2023-08-23 12:45:44 +0100

    creation des modeles et les tables associés

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,12 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Project extends Model
 {
     use HasFactory;
-
-    protected $table = 'users';
-
-    
+    protected $table = 'projects';
 
 }
